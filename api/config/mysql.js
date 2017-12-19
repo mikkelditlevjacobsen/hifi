a5b69b51a35aa3e04bb07b4735084775b8fe334f
mikkelditlevjacobsen <mikkel@jdadm.dk>
2017-12-19 09:41:57 +0100

    ik

--- api/config/mysql.js
+++ api/config/mysql.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2'); // mysql
 module.exports = {
    connect: function () {
       return mysql.createConnection({
-         host: 'localhost',
+         host: '188.226.185.108',
          user: 'root',
          password: 'cra57rze',
          database: 'hifi'
